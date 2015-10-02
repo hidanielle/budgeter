75e5c615940ebec7b52bdf1436af24985235607e
Danielle.Graham <daniellecg4@gmail.com>
2015-10-02 15:16:19 -0400

    Add some text and general layout tweaks

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,56 +9,7 @@
  */
 angular.module('budgeterApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.items = [
-      {
-        'id': 1,
-        'type': 'add',
-        'category': 'income',
-        'name': 'Monthly Income',
-        'amount': 1500
-      },
-      {
-        'id': 2,
-        'type': 'subtract',
-        'category': 'debt',
-        'name': 'Student Loan',
-        'amount': 200
-      },
-      {
-        'id': 3,
-        'type': 'subtract',
-        'category': 'debt',
-        'name': 'Visa',
-        'amount': 150
-      },
-      {
-        'id': 4,
-        'type': 'subtract',
-        'category': 'bills',
-        'name': 'Cell Phone',
-        'amount': 90
-      },
-      {
-        'id': 5,
-        'type': 'subtract',
-        'category': 'lifestyle',
-        'name': 'Gym Membership',
-        'amount': 45
-      },
-      {
-        'id': 6,
-        'type': 'subtract',
-        'category': 'lifestyle',
-        'name': 'Groceries',
-        'amount': 200
-      },
-      {
-        'id': 7,
-        'type': 'subtract',
-        'category': 'savings',
-        'amount': 15
-      }
-    ];
+    $scope.items = [];
 
     $scope.addItem = function() {
       $scope.editorEnabled = false;
@@ -85,30 +36,32 @@ angular.module('budgeterApp')
         });
       }
       $scope.item = {};
+      $scope.total();
     };
 
     $scope.removeItem = function(item) {
       $scope.items.splice($scope.items.indexOf(item),1);
+      $scope.total();
     };
 
     $scope.totalAdd = 0;
     $scope.totalSubtract = 0;
     $scope.totalSavings = 0;
 
-    angular.forEach($scope.items, function(item) {
-      if (item.type==='add') {
-        $scope.totalAdd += item.amount;
-      }
-      if (item.type==='subtract') {
-        $scope.totalSubtract += item.amount;
-      }
-      if (item.category === 'savings') {
-        $scope.totalSavings += item.amount;
-      }
-    });
-
-    $scope.labels = ["Income", "Expenses", "Savings"];
-    $scope.data = [$scope.totalAdd, $scope.totalSubtract, $scope.totalSavings];
-
+    $scope.total = function() {
+      angular.forEach($scope.items, function(item) {
+        if (item.type==='add') {
+          $scope.totalAdd += item.amount;
+        }
+        if (item.type==='subtract') {
+          $scope.totalSubtract += item.amount;
+        }
+        if (item.category === 'savings') {
+          $scope.totalSavings += item.amount;
+        }
+      });
+      $scope.labels = ["Income", "Expenses", "Savings"];
+      $scope.data = [$scope.totalAdd, $scope.totalSubtract, $scope.totalSavings];
+    };
 
   }]);
